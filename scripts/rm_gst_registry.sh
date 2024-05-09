f2662b7e32f0796be4cc871dc8c0a5325a9acde5
John Newton <johnnewto@gmail.com>
2024-05-10 10:33:19 +1200

    Update rm_gst_registry.sh to remove Gstreamer cache files based on platform

--- scripts/rm_gst_registry.sh
+++ scripts/rm_gst_registry.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
-file_path="/home/john/.cache/gstreamer-1.0/registry.x86_64.bin"
+file_path_x86="/home/$USER/.cache/gstreamer-1.0/registry.x86_64.bin"
+file_path_aarch64="/home/$USER/.cache/gstreamer-1.0/registry.aarch64.bin"
 
-if [ -f "$file_path" ]; then
-    rm "$file_path"
-    echo "Gstreamer cache file removed successfully."
+if [ "$(uname -m)" == "x86_64" ]; then
+    if [ -f "$file_path_x86" ]; then
+        rm "$file_path_x86"
+        echo "Gstreamer cache file for x86 removed successfully."
+    else
+        echo "File for x86 does not exist."
+    fi
+elif [ "$(uname -m)" == "aarch64" ]; then
+    if [ -f "$file_path_aarch64" ]; then
+        rm "$file_path_aarch64"
+        echo "Gstreamer cache file for aarch64 removed successfully."
+    else
+        echo "File for aarch64 does not exist."
+    fi
 else
-    echo "File does not exist."
+    echo "Unsupported platform."
 fi
 
